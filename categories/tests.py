a6fcd9a91c42a0f4cb4407857a3cd69b69a64bc7
r4g309 <bscortes94@ucatolica.edu.co>
2024-03-03 22:55:01 -0500

    test for user and categories

--- categories/tests.py
+++ categories/tests.py
@@ -1,3 +1,44 @@
 from django.test import TestCase
+from rest_framework.test import APIClient
+
+from .models import Category
+
 
 # Create your tests here.
+class CategoryTestCase(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.category = Category.objects.create(
+            name="test",
+            description="test",
+        )
+
+    def test_get_category(self):
+        response = self.client.get("/category/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_create_category(self):
+        response = self.client.post(
+            "/category/",
+            {
+                "name": "test1",
+                "description": "test",
+            },
+            format="json",
+        )
+        self.assertEqual(response.status_code, 201)
+
+    def test_update_category(self):
+        response = self.client.put(
+            f"/category/{self.category.name}/",
+            {
+                "name": "test",
+                "description": "test",
+            },
+            format="json",
+        )
+        self.assertEqual(response.status_code, 200)
+
+    def test_delete_category(self):
+        response = self.client.delete(f"/category/{self.category.name}/")
+        self.assertEqual(response.status_code, 204)
--- user/tests.py
+++ user/tests.py
@@ -1,3 +1,88 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
+from rest_framework.test import APIClient
+
 
 # Create your tests here.
+class UserTestCase(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = User.objects.create(
+            username="test",
+            email="b@mail.com",
+            password="test",
+            first_name="test",
+            last_name="test",
+        )
+
+    def test_create_user(self):
+        response = self.client.post(
+            "/register",
+            {
+                "username": "rage",
+                "email": "b@mail.com",
+                "password": "1234",
+                "first_name": "Brayan",
+                "last_name": "Cortes",
+            },
+            format="json",
+        )
+        self.assertEqual(response.status_code, 201)
+
+    def test_login_user(self):
+        response = self.client.post(
+            "/register",
+            {
+                "username": "user10",
+                "email": "b@mail.com",
+                "password": "1234",
+                "first_name": "User",
+                "last_name": "Test",
+            },
+            format="json",
+        )
+        self.assertEqual(response.status_code, 201)
+        response = self.client.post(
+            "/login",
+            {
+                "username": "user10",
+                "password": "1234",
+            },
+            format="json",
+        )
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_user(self):
+        response = self.client.post(
+            "/register",
+            {
+                "username": "user10",
+                "email": "b@mail.com",
+                "password": "1234",
+                "first_name": "User",
+                "last_name": "Test",
+            },
+            format="json",
+        )
+        self.assertEqual(response.status_code, 201)
+        user_tk = response.data.get("token")
+        response = self.client.get("/profile", headers={"Authorization": f"Token {user_tk}"})
+        self.assertEqual(response.status_code, 200)
+
+    #
+    def test_logout_user(self):
+        response = self.client.post(
+            "/register",
+            {
+                "username": "user10",
+                "email": "b@mail.com",
+                "password": "1234",
+                "first_name": "User",
+                "last_name": "Test",
+            },
+            format="json",
+        )
+        self.assertEqual(response.status_code, 201)
+        user_tk = response.data.get("token")
+        response = self.client.get("/logout", headers={"Authorization": f"Token {user_tk}"})
+        self.assertEqual(response.status_code, 200)
